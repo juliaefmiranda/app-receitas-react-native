c0cf9d43ec20e5a628257a0196041a245b3cecf7
julia <jerlo180908@gmail.com>
2026-03-20 14:50:34 -0300

    Implementa adição e remoção de receitas

--- App.js
+++ App.js
@@ -1,7 +1,37 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, Text, View, FlatList, TextInput, TouchableOpacity } from 'react-native';
 
 export default function App() {
+   
+  const [novaReceita, setNovaReceita] = useState("");
+
+  const [listaReceitas, setListaReceitas] = useState([]);
+
+  //Função para adicionar receita (spread operator)
+  const adicionarReceita = () => {
+    if (novaReceita.trim() === "") return;
+
+    const receitaObjeto = {
+      id: String(Date.now()),
+      texto: novaReceita,
+    };
+
+    //Pega tudo que já tinha lista (...listaReceitas) e joga o novo item no final
+    setListaReceitas([...listaReceitas, receitaObjeto]);
+
+    setNovaReceita("");
+  }
+
+  //Função para remover receita (filter)
+  const removerReceita = (idParaRemover) => {
+    const listaFiltrada = listaReceitas.filter(
+      (item) => item.id !== idParaRemover
+    );
+
+    setListaReceitas(listaFiltrada);
+  }
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
